b1929d1fe024b2dbdc57dbd8e5908a525279699b
Tim <TimCPB@github.com>
2020-09-02 15:42:38 +0100

    App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return(
+      <div classname="App">
+        <h1>Yo world!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
