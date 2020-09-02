7c6b5c259e2fe538622535bb394232ea9012f860
Tim <TimCPB@github.com>
2020-09-02 16:28:32 +0100

    moved script from head to body in App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import "./App.css";
 class App extends Component {
   render() {
     return(
-      <div classname="App">
+      <div className="App">
         <h1>Yo world!</h1>
       </div>
     );
